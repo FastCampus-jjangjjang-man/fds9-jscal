868492b9a81862ef3a267ff06220eadfea12a028
DongbeenSeo <puus1021@gmail.com>
2018-04-26 15:05:40 +0900

    display number

--- calculator.js
+++ calculator.js
@@ -1,1 +1,15 @@
 // 아름다운 코드
+const displayEl = document.querySelector('.display');
+
+const numbers = [];
+
+for (let i = 0; i <= 9; i++) {
+    numbers[i] = document.querySelector(`.num-${i}`);
+}
+
+
+numbers.forEach((el, index) => {
+    numbers[index].addEventListener('click', () => {
+        displayEl.textContent = index;
+    })
+})
