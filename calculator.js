18363e12ac515f1260aca7ab528a384772bc467d
DongbeenSeo <puus1021@gmail.com>
2018-04-26 17:07:57 +0900

    연산자 출력

--- calculator.js
+++ calculator.js
@@ -1,8 +1,11 @@
 // 아름다운 코드
 const displayEl = document.querySelector('.display');
-
+const operatorEl = document.querySelectorAll('.op');
 const numbers = [];
-
+let wholeStr = '';
+let beforeStr = '';
+let afterStr = '';
+let operatorStr = '';
 for (let i = 0; i <= 9; i++) {
     numbers[i] = document.querySelector(`.num-${i}`);
 }
@@ -10,6 +13,20 @@ for (let i = 0; i <= 9; i++) {
 
 numbers.forEach((el, index) => {
     numbers[index].addEventListener('click', () => {
-        displayEl.textContent = index;
+        wholeStr += numbers[index].textContent;
+        displayEl.textContent = wholeStr;
+    })
+})
+operatorEl.forEach((el, index) => {
+    el.addEventListener('click', () => {
+        if (el.textContent === '+' || el.textContent === '−' || el.textContent === '×' || el.textContent === '÷') {
+            operatorStr = el.textContent;
+            wholeStr += operatorStr;
+            displayEl.textContent = wholeStr;
+        }
+        if (el.textContent === 'AC') {
+            displayEl.textContent = '0';
+            wholeStr = '';
+        }
     })
 })
