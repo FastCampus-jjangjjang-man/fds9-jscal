74cfe7d97bde8c3752692708fcb49908f766c510
DongbeenSeo <puus1021@gmail.com>
2018-04-30 18:11:15 +0900

    연산자연속클릭이벤트 및 계산결과 출력

--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-// 아름다운 코드
 const displayEl = document.querySelector('.display');
 const operatorEl = document.querySelectorAll('.op');
 const numbers = [];
@@ -10,23 +9,86 @@ for (let i = 0; i <= 9; i++) {
     numbers[i] = document.querySelector(`.num-${i}`);
 }
 
+function operationFunc(resultStr, str) {
+
+    let result = 0;
+    let arr = resultStr.split(str);
+    if (arr[0] === '') {
+        arr[0] = 0;
+    }
+    switch (str) {
+        case '+':
+            result = parseInt(arr[0]) + parseInt(arr[1]);
+            break;
+        case '−':
+            result = parseInt(arr[0]) - parseInt(arr[1]);
+            break;
+        case '×':
+            result = parseInt(arr[0]) * parseInt(arr[1]);
+            break;
+        case '÷':
+            result = parseInt(arr[0]) / parseInt(arr[1]);
+            break;
+        default:
+            break;
+    }
+    return result;
+}
+
+let cntOperatorClick = 0;
+let wholeStrLen = 0;
 
 numbers.forEach((el, index) => {
-    numbers[index].addEventListener('click', () => {
-        wholeStr += numbers[index].textContent;
+    el.addEventListener('click', () => {
+        console.log(`wholeStr: ${wholeStr}`);
+        wholeStr += el.textContent;
+        console.log(`wholeStr: ${wholeStr}`);
         displayEl.textContent = wholeStr;
+        cntOperatorClick = 0;
+        wholeStrLen = wholeStr.length;
     })
 })
+
+let calResult = 0;
 operatorEl.forEach((el, index) => {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', e => {
         if (el.textContent === '+' || el.textContent === '−' || el.textContent === '×' || el.textContent === '÷') {
+            cntOperatorClick++;
+            if (cntOperatorClick === 1 && operatorStr !== '') {
+                console.log(operatorStr);
+                console.log(wholeStr);
+                calResult = operationFunc(wholeStr, operatorStr);
+                wholeStr = calResult;
+                console.log(`calResult: ${calResult}`);
+            }
             operatorStr = el.textContent;
-            wholeStr += operatorStr;
+            //연산자 2개 연속 눌렸을때
+            if (cntOperatorClick >= 2) {
+                wholeStr = wholeStr.replace(wholeStr[wholeStrLen - 1], operatorStr);
+            } else {
+                wholeStr += operatorStr;
+            }
             displayEl.textContent = wholeStr;
-        }
-        if (el.textContent === 'AC') {
+
+        } else if (el.textContent === 'AC') {
             displayEl.textContent = '0';
             wholeStr = '';
+        } else if (el.textContent === '=') {
+            //'=' click하면 계산결과 출력
+            calResult = operationFunc(wholeStr, operatorStr)
+            wholeStr = '';
+            if (calResult === 0) {
+                wholeStr = 0;
+                displayEl.textContent = 0;
+                console.log(`wholeStr: ${wholeStr}`);
+            } else {
+                wholeStr = calResult;
+                displayEl.textContent = wholeStr;
+                console.log(`wholeStr: ${wholeStr}`);
+            }
+            operatorStr = '';
+            cntOperatorClick = 0;
         }
+        wholeStrLen = wholeStr.length;
     })
 })
